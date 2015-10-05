0bfb57e58a59fc8b64c04f5e128e539760333790
Feras Deiratany <deiratany.1@osu.edu>
2015-10-05 02:24:30 -0400

    nokogiri instead of mechanize

--- get_oclc.rb
+++ get_oclc.rb
@@ -11,24 +11,43 @@ File: get_oclc.rb
 =end
 
 require 'rubygems'
-require 'mechanize'
+# require 'mechanize'
+require 'nokogiri'
+require 'open-uri'
 
 # Start of main 
 base_url = "http://library.ohio-state.edu/search/c?SEARCH="
-# Ask for call numner file
+# Ask for call number file
 print "Please enter call number file: "
 fileName = gets.chomp
 
 out_file = File.new("out.txt", "w")
-
+oclc = ""
 f = File.open(fileName, "r") 
 f.each_line do |line|
   line = line.gsub(' ', '+')
   url = "#{base_url}#{line}"
-  agent = Mechanize.new
-  page = agent.get(url)
-  #get oclc number (there are two on each page)
-  out_file.puts page.links_with(:href => %r{\/search~S7\?\/o})
+  # using Nokogiri instead of Mechanize 
+  # agent = Mechanize.new
+  # page = agent.get(url)
+
+  doc = Nokogiri::HTML(open(url))
+
+  if doc.at_css('table.browseList')  
+    out_file.puts 'manual' #need to figure out link clicking
+  else
+    doc.xpath('//a[@href]').each do |link|
+      if link['href'].include? "/search~S7?/o"
+        oclc = link.text.strip
+      end
+    end
+    out_file.puts oclc
+  end
+
+  #   # old mechanize method with regex
+  #   #get oclc number (there are two on each page)
+  #   x = page.links_with(:href => %r{\/search~S7\?\/o})
+  #   out_file.puts x
 end
 f.close
 out_file.close
