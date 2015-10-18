49671e650fe8befd5ca92906a48d4f547a485dcd
Feras Deiratany <deiratany.1@osu.edu>
2015-10-18 17:32:53 -0400

    check if record found

--- main.rb
+++ main.rb
@@ -24,11 +24,25 @@ def main
   # Get OSU Search by LC Library page
   page = agent.get "http://library.ohio-state.edu/search/c"
 
+  # Search each record
   search_form = page.form_with :class => "unpadded"
-  search_form.field_with(:name => "SEARCH").value = "BC117.T8 A35 1886"
+  search_form.field_with(:name => "SEARCH").value = "AC141 .K54 1911"
 
   search_results = agent.submit search_form
-  puts search_results.body
+
+  # Nokogiri 
+  resultPage = Nokogiri::HTML(search_results.body)
+
+  # get error class
+  error = resultPage.css("tr[class='msg']")
+  
+  # if no matches found 
+  if error.text.to_s.eql? "No matches found; nearby LC CALL NOS are:"
+    puts 'no matches found'
+  else 
+    puts 'found'
+  end
+
 end
 
 main
