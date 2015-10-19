23d4f38498ae14baba259cc254d602be489c1625
Feras Deiratany <deiratany.1@osu.edu>
2015-10-18 21:56:45 -0400

    get lc records from file

--- main.rb
+++ main.rb
@@ -15,6 +15,13 @@ require 'nokogiri'
 
 
 def main
+  fileName = "test_data.txt"
+  # open file to read from
+  f = File.open(fileName, "r")
+  
+  # open file to write to
+  # out_file = File.new("out.txt", "w")
+
   # Initialize new Mechanize agent
   agent = Mechanize.new
   
@@ -24,24 +31,27 @@ def main
   # Get OSU Search by LC Library page
   page = agent.get "http://library.ohio-state.edu/search/c"
 
-  # Search each record
-  search_form = page.form_with :class => "unpadded"
-  search_form.field_with(:name => "SEARCH").value = "AC141 .S94 1918"
+  # get call numbers from file
+  f.each_line do |lc_record|
+    # Search each record
+    search_form = page.form_with :class => "unpadded"
+    search_form.field_with(:name => "SEARCH").value = lc_record
 
-  search_results = agent.submit search_form
+    search_results = agent.submit search_form
 
-  # Nokogiri 
-  resultPage = Nokogiri::HTML(search_results.body)
-  
-  # if no matches found 
-  if resultPage.css("tr[class='msg']").text.to_s.eql? "No matches found; nearby LC CALL NOS are:"
-    puts 'no matches found'
-  # if multiple records were found
-  elsif resultPage.at_css("table[class='browseList']")
-    puts 'found'
-  # if on record is found
-  elsif resultPage.css("a[id='recordnum']").text.to_s.eql? "Permanent link to this record"
-    puts 'found one record'
+    # Nokogiri 
+    resultPage = Nokogiri::HTML(search_results.body)
+    
+    # if no matches found 
+    if resultPage.css("tr[class='msg']").text.to_s.eql? "No matches found; nearby LC CALL NOS are:"
+      puts 'no matches found'
+    # if multiple records were found
+    elsif resultPage.at_css("table[class='browseList']")
+      puts 'found'
+    # if on record is found
+    elsif resultPage.css("a[id='recordnum']").text.to_s.eql? "Permanent link to this record"
+      puts 'found one record'
+    end
   end
 end
 
